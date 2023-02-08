e96949d7a257e619e6a1ff34d84d1c1c9872443e
Felipe Valeriano <101677047+FelipeValeriano21@users.noreply.github.com>
2023-02-08 09:28:04 -0300

    Add files via upload

---
+++ style/script/profile.js
@@ -0,0 +1,20 @@
+onload = mudacorbordapreta (); 
+
+
+
+function mudacorbordapreta () {
+
+    document.getElementById('felipefoto').style.borderTopColor = "yellow"
+    document.getElementById('felipefoto').style.borderBottomColor = "yellow"
+    document.getElementById('felipefoto').style.borderInlineColor = "black"
+    mudacorbordaamarela ();
+
+}
+
+function mudacorbordaamarela () {
+
+   
+
+    mudacorbordapreta ();
+
+}
